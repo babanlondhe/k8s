80ec866e00dacec1c06928d72a7a26cb661da2ef
babanlondhe <120760271+babanlondhe@users.noreply.github.com>
2023-03-17 21:03:28 +0530

    main.tf

--- nain.tf
+++ nain.tf
@@ -1,14 +1,39 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAZA524WNNNCZ53LOV"
-  secret_key = "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"
+   region = "us-east-1"
 }
-
-resource "aws_instance" "ec2" {
-  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t2.micro"
-  key_name = "demokey" 
-  tags = {
-    Name = "HelloWorld"
+  resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "myvpc"
+    }
   }
-}
+
+  resource "aws_subnet" "mysub" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.1.0/24"
+  }
+  
+  resource "aws_instance" "mynew" {
+    ami = "ami-005f9685cb30f234b"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.mysub.id
+    key_name = "TF_key"
+    tags = {
+      Name = "HelloWorld"
+    }
+  }
+  
+  resource "aws_key_pair" "TF_key" {
+    key_name = "TF_key"
+    public_key = tls_private_key.rsa.public_key_openssh
+  }
+
+  resource "tls_private_key" "rsa" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+  }
+
+  resource "local_file" "TF-key" {
+    content = tls_private_key.rsa.private_key_pem
+    filename = "tfkey"
+  }
